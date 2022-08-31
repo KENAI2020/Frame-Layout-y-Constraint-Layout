aa355e279947ce8e2d614909a79b31b37e662e52
Kenai2020 <mauricioquiroz@10>
2022-08-31 11:10:09 -0400

    subiendo tarea

---
+++ app/src/main/java/com/example/frameyconstraint/ConstrainLayout.kt
@@ -0,0 +1,11 @@
+package com.example.frameyconstraint
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConstrainLayout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constrain_layout)
+    }
+}
